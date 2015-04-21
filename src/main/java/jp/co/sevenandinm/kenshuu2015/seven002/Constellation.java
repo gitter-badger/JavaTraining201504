51d8e36d0265f3729df4053e7ec9f72f39e06747
newemployee2 <newemployee2@EPC2002>
2015-04-21 13:19:07 +0900

    refs #1 星座のスーパークラス

---
+++ src/main/java/jp/co/sevenandinm/kenshuu2015/seven002/Constellation.java
@@ -0,0 +1,62 @@
+package jp.co.sevenandinm.kenshuu2015.seven002;
+
+public class Constellation {
+
+
+	protected int start_day  = 0;
+	protected int start_month = 0;
+	protected int end_month = 0;
+	protected int end_day = 0;
+	protected String Japanese_name = "";
+
+
+		public int getStart_month() {
+		return start_month;
+	}
+
+	public void setStart_month(int start_month) {
+		this.start_month = start_month;
+	}
+
+
+	public int getStart_day() {
+		return start_day;
+	}
+
+	public void setStart_day(int start_day) {
+		this.start_day = start_day;
+	}
+
+
+	public int getEnd_month() {
+		return end_month;
+	}
+
+	public void setEnd_month(int end_month) {
+		this.end_month = end_month;
+	}
+
+
+	public int getEnd_day() {
+		return end_day;
+	}
+
+	public void setEnd_day(int end_day) {
+		this.end_day = end_day;
+	}
+
+
+
+	public String getJapanese_name() {
+		return Japanese_name;
+	}
+
+	public void setJapanese_name(String japanese_name) {
+		this.Japanese_name = japanese_name;
+	}
+
+
+
+
+
+	}
