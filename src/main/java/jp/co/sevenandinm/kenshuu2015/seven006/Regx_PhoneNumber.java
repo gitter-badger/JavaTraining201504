8a69c860b7fb6d76676413083269ceb58685f541
newemployee6@EPC2006 <yuyfujii@7andinm.co.jp>
2015-04-22 16:47:20 +0900

    refs #2 tellnumber check byfujii

--- src/main/java/jp/co/sevenandinm/kenshuu2015/seven006/Regx_PhoneNumber.java
+++ src/main/java/jp/co/sevenandinm/kenshuu2015/seven006/Regx_PhoneNumber.java
@@ -24,7 +24,10 @@ public class Regx_PhoneNumber{
 
 	}
 	public static boolean excude(String str){
-		return str.matches("[0-9][0-9]-[0-9]{4}-[0-9]{4}|[0-9]{4}-[0-9]{2}-[0-9]{4}|[0-9]{3}-[0-9]{3}-[0-9]{4}");
+		String tel244="[0-9]{2}-[0-9]{4}-[0-9]{4}|";
+		String tel424="[0-9]{4}-[0-9]{2}-[0-9]{4}|";
+		String tel334="[0-9]{3}-[0-9]{3}-[0-9]{4}";
+		return str.matches(tel244+tel424+tel334);
 	}
 	public static void  dispOut(boolean flag){
 		if(flag){
