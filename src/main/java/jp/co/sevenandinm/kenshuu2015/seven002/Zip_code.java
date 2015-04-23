9bd769aff43dadb0d1fb1a3f27945b042e7ab0a3
seven002@EPC2002 <shiakaza@7andinm.co.jp>
2015-04-23 09:33:05 +0900

    郵便番号　赤崎

---
+++ src/main/java/jp/co/sevenandinm/kenshuu2015/seven002/Zip_code.java
@@ -0,0 +1,42 @@
+package jp.co.sevenandinm.kenshuu2015.seven002;
+
+import java.util.Scanner;
+
+public class Zip_code {
+
+	public static void main(String[] args) {
+
+		Zip_code my = new Zip_code();
+
+		String str = inputData();
+		boolean flag = execute(str);
+		my.dispOut(flag);
+
+	}
+
+	private static boolean execute(String str) {
+		return str.matches("^\\d{3}-\\d{4}$");
+	}
+
+	private static java.lang.String inputData() {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("電話番号を入力してください");
+		Scanner stdIn = new Scanner(System.in);
+		String int_phonenum = stdIn.nextLine();
+		return int_phonenum;
+	}
+
+	private void dispOut(boolean flag) {
+		// TODO 自動生成されたメソッド・スタブ
+		if (flag) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+	}
+
+	// String maches = ("^0\\d{1,2}-\\d{1,4}-\\d{4}$");
+
+}
+
+
