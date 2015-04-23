177b59859cf2786e25c8d3b20b4aa0d0e32c045c
newenployee7@epc2007 <kenyamag@7andinm.co.jp>
2015-04-23 09:54:52 +0900

    refs #2 山岸 電話番号判別

---
+++ src/main/java/jp/co/sevenandinm/kenshuu2015/seven007/Regx_Mail.java
@@ -0,0 +1,29 @@
+/**
+ *
+ */
+package jp.co.sevenandinm.kenshuu2015.seven007;
+
+/**
+ * @author newenployee7
+ *
+ */
+public class Regx_Mail {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
+
+
+/*
+name	return
+class name	Regx_Mail
+入力値取得メソッド	inputData	String
+電話番号判定処理	execute	boolean
+結果出力処理	dispOut	void
+*/
--- src/main/java/jp/co/sevenandinm/kenshuu2015/seven007/Regx_PhoneNumber.java
+++ src/main/java/jp/co/sevenandinm/kenshuu2015/seven007/Regx_PhoneNumber.java
@@ -5,47 +5,23 @@ package jp.co.sevenandinm.kenshuu2015.seven007;
 
 import java.util.Scanner;
 
-/**
- * @author newenployee7
- *
- */
+//入力された電話番号のフォーマットが正しいか判別するプログラム
 public class Regx_PhoneNumber {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 
-		System.out.println("電話番号を入力してください(例 **-****-****)");
+		System.out.println("電話番号を入力してください (例 **-****-**** or ***-****-**** or ****-**-****)");
 		Scanner stdIn = new Scanner(System. in);
 		String put = stdIn.nextLine();
 		boolean dispOut = inputData(put);
-		if( dispOut == true){
+		if( dispOut == true ){
 			System.out.println("入力された電話番号(" +put +")は正しいです");
 		}else{
 			System.out.println("入力された電話番号(" +put +")は不正です");
 		}
 	}
 
-
 	private static boolean inputData(String input){
-		boolean execute = input.matches("[0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]");
+		boolean execute = input.matches("[0-9]{2,4}-[0-9]{3,4}-[0-9]{4}");
 		return execute;
 	}
-
 }
-
-
-/*Scannerで入力された文字が、電話番号であるかどうか判定し、
-あっていれば、「電話番号として正しいです。」
-間違っていれば、「電話番号ではありません」
-と表示するプログラムを作成しなさい。
-
-
-
-class name	Regx_PhoneNumber
-入力値取得メソッド	inputData
-電話番号判定処理	execute
-結果出力処理	dispOut
-*/
